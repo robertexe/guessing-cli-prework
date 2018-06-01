e605fbf4530ac847ea01647d9205340d695af9b6
Robert Tabares <robtabares92@gmail.com>
2018-06-01 18:03:47 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,8 +4,8 @@ def run_guessing_game
   generated_num = rand(1..6)
   user_input = gets.chomp
     if user_input == "exit"
-
-      return puts "Thanks for playing! bye bye!"
+      puts "Thanks for playing! bye bye!"
+      break
     elsif user_input.to_i == generated_num
       puts "You guessed the correct number!"
     else
