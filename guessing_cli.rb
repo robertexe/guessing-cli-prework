945d08a0b509b10bdfe9048a457296fa389e4e92
Robert Tabares <robtabares92@gmail.com>
2018-06-01 17:40:14 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -24,7 +24,9 @@ def running_guessing_game
   puts "Guess a number between 1 and 6."
   if generated_number == user_guess
     "You guessed the correct number!"
-  elsif generated_number != user_guess and user_guess == "exit"
-    exit 
+  elsif generated_number != user_guess
+    "The computer guessed #{generated_number}"
+  elsif user_guess == "exit"
+    break
   end
 end
